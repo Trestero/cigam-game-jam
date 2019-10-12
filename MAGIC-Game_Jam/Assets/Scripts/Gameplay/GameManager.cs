d3a4ed906431ebb20feabd43ee2260df8ba49f62
tps3871 <tps3871@rit.edu>
2019-10-12 19:54:55 -0400

    Fixed main camera picking up non-UI elements

--- MAGIC-Game_Jam/Assets/Scripts/Gameplay/GameManager.cs
+++ MAGIC-Game_Jam/Assets/Scripts/Gameplay/GameManager.cs
@@ -62,7 +62,6 @@ public class GameManager : MonoBehaviour
             {
                 gameState = GameState.ALIVE;
                 playerEarth.transform.position = playerEarthPosition;
-            }
                 camRig.ScreenRatio = 0.5f;
             }
         }
