d7ac95c0b4a7495cbd75b765b6556cf2928cdc16
Drake Walker Richards (RIT Student) <dwr2966@ad.rit.edu>
2019-10-12 19:12:48 -0400

    working on implementing geyser

---
+++ MAGIC-Game_Jam/Assets/Scripts/General Objects/Geyser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour
+{
+    private float waitTime = 1.5f;
+    private float wTimer = 0.0f;
+    private float geyserTime = 2.0f;
+    private float gTimer = 0.0f;
+    private bool isActivate = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isActivate)
+        {
+            wTimer += Time.deltaTime;
+        }
+        else
+        {
+            gTimer += Time.deltaTime;
+            transform.localScale += new Vector3(0.0f, 0.1f, 0.0f);
+        }
+        if(wTimer > waitTime)
+        {
+            wTimer -= waitTime;
+            isActivate = true;
+        }
+        if (gTimer > geyserTime)
+        {
+            gTimer -= geyserTime;
+            isActivate = false;
+        }
+    }
+}
