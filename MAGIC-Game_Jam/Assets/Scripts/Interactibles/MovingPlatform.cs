8b644e28a03001630f33151d711af7370d72b968
nlr3026 <nlr3026@rit.edu>
2019-10-13 23:49:51 -0400

    bleh

--- MAGIC-Game_Jam/Assets/Scripts/Interactibles/MovingPlatform.cs
+++ MAGIC-Game_Jam/Assets/Scripts/Interactibles/MovingPlatform.cs
@@ -6,23 +6,21 @@ public class MovingPlatform : MonoBehaviour
 {
     Vector3[] bounds;
     [SerializeField] float speed;
-    Vector3 direction;
     GameObject player;
+    float timer = 0;
+    bool lerpUp = true;
 
     // Start is called before the first frame update
     void Start()
     {
         //get positions of the bounds
         bounds = new Vector3[2];
-        bounds[0] = gameObject.GetComponentInParent<Transform>().position;
-        bounds[1] = transform.parent.GetChild(0).transform.position;
+        bounds[0] = transform.parent.GetChild(0).transform.position;
+        bounds[1] = transform.parent.GetChild(1).transform.position;
+        Debug.Log(bounds);
 
         //snap platform to the center point of it's movement
-        Vector3 startingPos = (bounds[0] + bounds[1]) / 2;
-        transform.position = startingPos;
-
-        direction = Vector3.Normalize(bounds[1] - transform.position);
-
+        transform.position = bounds[0];
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -34,16 +32,21 @@ public class MovingPlatform : MonoBehaviour
 
     public void Move()
     {
-        //past bounds
-        if(!(transform.position.x > bounds[0].x && transform.position.x < bounds[1].x))
+        if (lerpUp)
         {
-            Debug.Log("lower: " + bounds[0]);
-            Debug.Log("upper: " + bounds[1]);
-            Debug.Log("position: " + transform.position);
-            direction *= -1;
+            timer += Time.deltaTime * speed;
+        }
+        else
+        {
+            timer -= Time.deltaTime * speed;
+        }
+        transform.position = Vector3.Lerp(bounds[0], bounds[1], timer);
+
+        if(timer >= 1 || timer <= 0)
+        {
+            lerpUp = !lerpUp;
         }
 
-        transform.position += direction * speed;
     }
 
     private void OnCollisionEnter(Collision collision)
