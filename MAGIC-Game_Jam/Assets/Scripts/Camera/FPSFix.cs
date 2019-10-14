c041a736224ffed51474d1f7e8a344ddbbb23b5a
tps3871 <tps3871@rit.edu>
2019-10-14 13:05:24 -0400

    Fixed VSync issues

---
+++ MAGIC-Game_Jam/Assets/Scripts/Camera/FPSFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// VSync-based issues solution found at https://answers.unity.com/questions/587571/standalone-builds-run-very-fast-on-some-computers.html
+
+public class FPSFix : MonoBehaviour
+{
+    private Resolution res;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        res = Screen.currentResolution;
+
+        if(res.refreshRate == 60)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        if (res.refreshRate == 120)
+        {
+            QualitySettings.vSyncCount = 2;
+        }
+    }
+
+}
