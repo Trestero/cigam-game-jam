f945c560e53c146820c3436543e19219ebcb7d7b
tps3871 <tps3871@rit.edu>
2019-10-13 23:39:04 -0400

    Set up finish line prefab

---
+++ MAGIC-Game_Jam/Assets/Scripts/Gameplay/FinishLine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1)%SceneManager.sceneCount);
+    }
+}
